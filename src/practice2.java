4e97827f2eaf8a9594f2ac2cc8e373db40384fbd
jingkie07 <jingkiealipuyo68@gmail.com>
2025-08-12 17:38:46 +0800

    ga add na sd ko ug lain HAHAHHAHAH

--- src/practice2.java
+++ src/practice2.java
@@ -3,20 +3,13 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package practice;
 
 /**
  *
  * @author Jingkie
  */
-public class Practice {
-
-    /**
-     * @param args the command line arguments
-     */
+public class practice2 {
     public static void main(String[] args) {
-        // TODO code application logic here
-        System.out.println("practise ni");
+        System.out.println("Hello Kara");
     }
-    
 }
